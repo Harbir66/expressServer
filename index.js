c5d78d313bbb040b483276389e0b439f007f379a
Harbir Singh <harbir066@gmail.com>
2023-01-26 22:49:25 +0530

    feat : add put request handler

--- index.js
+++ index.js
@@ -5,45 +5,45 @@ app.use(express.json());
 
 const port = 3000;
 
-const tasks=[];
-const tempTask={
-  'name':'abc',
-  'id':0,
-  'isComplete':true
+const tasks = [];
+const tempTask = {
+  name: 'abc',
+  id: 0,
+  isComplete: true,
 };
 tasks.push(tempTask);
-let index=0;
+let index = 0;
 
-
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log('Listening on port 3000...');
 });
 
 // GET methods
 
-app.get('/tasks',(req,res)=>{
+app.get('/tasks', (req, res) => {
   console.log('Get all tasks');
   res.status(200).json(tasks);
 });
-app.get('/tasks/:id',(req,res)=>{
+
+app.get('/tasks/:id', (req, res) => {
   console.log('Get task by id');
-  const requiredTaskIndex = tasks.findIndex(task => task.id === Number(req.params.id));
-  if(requiredTaskIndex===-1)
-  {
+  const { id } = req.params;
+  const requiredTaskIndex = tasks.findIndex((task) => task.id === Number(id));
+  if (requiredTaskIndex === -1) {
     res.status(404).send('Task Not Found').end();
-  } 
+  }
   res.status(200).send(tasks[requiredTaskIndex]);
 });
 
 // POST methods
 
-app.post('/tasks',(req,res)=>{
+app.post('/tasks', (req, res) => {
   console.log('Post a task');
-  index+=1;
-  const newTask={
-    'name':req.body.name,
-    'id':index,
-    'isComplete':false
+  index += 1;
+  const newTask = {
+    ...req.body,
+    id: index,
+    isComplete: false,
   };
   tasks.push(newTask);
   res.status(201).json(tasks);
@@ -51,15 +51,17 @@ app.post('/tasks',(req,res)=>{
 
 // PUT methods
 
-app.put('/tasks',(req,res)=>{
+app.put('/tasks', (req, res) => {
   console.log('Updating entire task');
-  const requiredTaskIndex = tasks.findIndex(task => task.id === Number(req.body.id));
-  if(requiredTaskIndex === -1){
+  const { id, name, isComplete } = req.body;
+  const requiredTaskIndex = tasks.findIndex((task) => task.id === Number(id));
+  if (requiredTaskIndex === -1) {
     res.status(404).send('Task not found');
   }
-  tasks[requireTaskIndex]['name']=req.body.name;
-  tasks[requireTaskIndex]['isComplete']=req.body.isComplete;
-  
-  res.status(200).json(tasks[requireTaskIndex]).end();
-  
+  tasks[requiredTaskIndex]['name'] = name;
+  tasks[requiredTaskIndex]['isComplete'] = isComplete;
+
+  res.status(200).json(tasks[requiredTaskIndex]).end();
 });
+
+
